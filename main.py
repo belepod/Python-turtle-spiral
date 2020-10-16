ed725e1c2f9d56cbdbc337dc93e6e155277bb7d4
Siddharth Arya <66844304+belepod@users.noreply.github.com>
2020-10-16 22:17:13 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+colors = ['red', 'yellow', 'green', 'purple', 'blue', 'orange']
+
+t = turtle.Pen()
+
+t.speed(100)
+
+turtle.bgcolor("black")
+
+for x in range(1000):
+	t.pencolor(colors[x%6])
+	t.width(x/100 + 1)
+	t.forward(x)
+	t.left(59)
+
+turtle.done()
